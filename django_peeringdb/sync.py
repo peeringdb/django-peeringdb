b68877f2111042bf61da6a0b77bc646c9305205f
Matt Griswold <grizz@20c.com>
2017-11-23 07:00:18 +0000

    remove cruft from sync

--- django_peeringdb/sync.py
+++ django_peeringdb/sync.py
@@ -46,7 +46,7 @@ def sync_obj(cls, row):
             setattr(obj, field.name, value.replace(tzinfo=None))
         else:
             if hasattr(field, "related_name"):
-                # skip many
+                # skip many, breaks updates to related fields
                 if field.multiple:
                     continue
                 # skip delete caused by None, see https://code.djangoproject.com/ticket/28742
@@ -55,22 +55,4 @@ def sync_obj(cls, row):
             setattr(obj, field.name, value)
 
     obj.save()
-    return
-
-    # times should always be UTC, so take off timezone since some dbs
-    # don't support it - FIXME make option
-    for field in cls._meta.get_fields():
-        #if isinstance(field, models.ForeignKey):
-        if isinstance(field, models.ForeignKey):
-            key = field.name + "_id"
-            value = getattr(obj, key, 0)
-        else:
-            key = field.name
-            value = getattr(obj, field.name, None)
-
-        if isinstance(value, datetime.datetime):
-            setattr(obj, field.name, value.replace(tzinfo=None))
-        else:
-            setattr(obj, field.name, value)
-
     return
