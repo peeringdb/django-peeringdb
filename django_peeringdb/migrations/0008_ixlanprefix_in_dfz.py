472cdeb914d1cd0db2a9fbf7af86d4ba0892f63a
Stefan Pratter <stefan@20c.com>
2020-05-25 08:23:58 +0000

    fix migration hierarchy

--- django_peeringdb/migrations/0008_ixlanprefix_in_dfz.py
+++ django_peeringdb/migrations/0008_ixlanprefix_in_dfz.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_peeringdb', '0006_net_operational'),
+        ('django_peeringdb', '0007_traffic_levels'),
     ]
 
     operations = [
