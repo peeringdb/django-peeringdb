6d3c9cb018a66a19ccaa7100f9012f03c5250f6e
Stefan Pratter <stefan@20c.com>
2021-07-20 12:32:08 +0000

    black format

--- django_peeringdb/admin/__init__.py
+++ django_peeringdb/admin/__init__.py
@@ -3,4 +3,4 @@ from django.conf import settings
 # we only set up admin views if concrete models are available
 
 if not settings.ABSTRACT_ONLY:
-    from django_peeringdb.admin.views import * # noqa
+    from django_peeringdb.admin.views import *  # noqa
