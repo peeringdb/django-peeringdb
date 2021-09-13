be4efb4840bd28e03ab49ed221ec1dd7c044ff08
Stefan Pratter <stefan@20c.com>
2021-09-13 19:25:32 +0300

    Support 2021-08 (#73)
    * multiplechoicefield and facility fields
    
    * changelog
    
    * linting
    
    * isort

--- tests/models.py
+++ tests/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-from django_peeringdb.models import LG_URLField, URLField
+from django_peeringdb.models import LG_URLField, URLField, MultipleChoiceField
 
 
 class FieldModel(models.Model):
     url = URLField(null=True, blank=True)
+    multichoice = MultipleChoiceField(
+        max_length=255,
+        null=True,
+        blank=True,
+        choices=[("1", "1"), ("2", "2"), ("3", "3")],
+    )
 
     class Meta:
         app_label = "django_peeringdb.tests"
